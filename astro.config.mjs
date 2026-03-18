e7b3e2579384e1556a4a79c3f50727cb8b62cf4f
MoYuan-CN <m56042764@163.com>
2026-03-18 20:41:05 +0800

    docs: upload chemistry/chapter-3/5/6 and concepts

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,15 +14,14 @@ import tailwindcss from "@tailwindcss/vite";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 
-const GoogleAnalyticsScript = 
-`
-    window.dataLayer = window.dataLayer || [];
-    function gtag() {
-        dataLayer.push(arguments);
-    }
-    gtag("js", new Date());
-    gtag("config", "G-GCCTMKM3TW");
-`
+// const GoogleAnalyticsScript = `
+//     window.dataLayer = window.dataLayer || [];
+//     function gtag() {
+//         dataLayer.push(arguments);
+//     }
+//     gtag("js", new Date());
+//     gtag("config", "G-GCCTMKM3TW");
+// `;
 
 // https://astro.build/config
 export default defineConfig({
@@ -32,16 +31,16 @@ export default defineConfig({
         starlight({
             title: "IntlEDU Docs",
             social: [{ icon: "github", label: "GitHub", href: "https://github.com/yun-websites/intledu-docs" }],
-            head: [
-                {
-                    tag: "script",
-                    attrs: { async: true, src: "https://www.googletagmanager.com/gtag/js?id=G-GCCTMKM3TW" },
-                },
-                {
-                    tag: "script",
-                    content: GoogleAnalyticsScript,
-                },
-            ],
+            // head: [
+            //     {
+            //         tag: "script",
+            //         attrs: { async: true, src: "https://www.googletagmanager.com/gtag/js?id=G-GCCTMKM3TW" },
+            //     },
+            //     {
+            //         tag: "script",
+            //         content: GoogleAnalyticsScript,
+            //     },
+            // ],
             defaultLocale: "zh-cn",
             locales: {
                 "en-gb": {
